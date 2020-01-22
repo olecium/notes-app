328624f612020473fa6118124a3fe00201769dba
Olesea C <emmy.gold@gmail.com>
2020-01-22 12:39:22 +0000

    Updates

---
+++ precache-manifest.d66961acbf590cff62c83c15fec435c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24a2b09efba20a3c08af6462c638b4f3",
+    "url": "/notes-app/index.html"
+  },
+  {
+    "revision": "93641a7a33a04e9b43b3",
+    "url": "/notes-app/static/css/main.830fd970.chunk.css"
+  },
+  {
+    "revision": "13783426b413798cc1e6",
+    "url": "/notes-app/static/js/2.7532094d.chunk.js"
+  },
+  {
+    "revision": "f50d2b340118642fdbf6d14486616590",
+    "url": "/notes-app/static/js/2.7532094d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "93641a7a33a04e9b43b3",
+    "url": "/notes-app/static/js/main.fc5dc503.chunk.js"
+  },
+  {
+    "revision": "2130a92484021e424dbc",
+    "url": "/notes-app/static/js/runtime-main.88f094ce.js"
+  }
+]);
